1e28c65e21cba112a07f95307525b57b460a969a
YanGraim <yangraim@gmail.com>
2025-01-07 11:36:13 -0300

    adiciona componente Header

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>
+      <img src="" alt="" />
+      <div>
+        <h1>Habitar</h1>
+        <span>A corretora de imóveis que conecta você ao seu novo lar</span>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
