452ba28d44bef61dcd7f2d7434ec04268ecb33b5
YanGraim <yangraim@gmail.com>
2025-01-20 23:23:56 -0300

    consome api

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-// BASE DA URL =
+// BASE DA URL = http://vps56643.publiccloud.com.br:3333
 // URL DA API =
 
 const api = axios.create({
-  baseURL: "",
+  baseURL: "/api",
 });
 
 export default api;
